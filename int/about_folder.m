f4d1d1a316dc5054a6c83d4ce4e35ba2371d9be0
Dani Schmid <dani.schmid@gmail.com>
2019-05-03 08:30:31 +0200

    about_folder.m  - corrected download location

--- int/about_folder.m
+++ int/about_folder.m
@@ -6,7 +6,7 @@ function about_folder
 % Last changed date:  $Date: 2015-09-07 15:26:14 +0200 (Pn, 07 wrz 2015) $
 %--------------------------------------------------------------------------
 
-msgbox({sprintf('This software is documented in the Journal of Structural Geology article: \n“Folder: A numerical tool to simulate the development of structures in layered media” \nby Marta Adamuszek, Marcin Dabrowski, and Daniel Walter Schmid.\n\nIt can be download from https://www.dropbox.com/sh/gvt4ucge5jkhvsn/AAD7K5wyQlv6IhRDSAaQa-8ua?dl=0.')},...
+msgbox({sprintf('This software is documented in the Journal of Structural Geology article: \n“Folder: A numerical tool to simulate the development of structures in layered media” \nby Marta Adamuszek, Marcin Dabrowski, and Daniel Walter Schmid.\n\nIt can be downloaded from github.com/dwschmid/folder.')},...
     'About Folder');
 
 end
